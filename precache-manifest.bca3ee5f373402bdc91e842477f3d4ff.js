fc1ee51d433461277b768b4103ded8135d2d3644
artsashko <artyomsashko@gmail.com>
2020-09-24 17:28:56 +0300

    Updates

---
+++ precache-manifest.bca3ee5f373402bdc91e842477f3d4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a15d57d792515fe5a9054976e087608",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "9c473db17d3348bf62a1",
+    "url": "/TodoApp/static/css/main.d183c019.chunk.css"
+  },
+  {
+    "revision": "bb1bbf2cc6aac51c0745",
+    "url": "/TodoApp/static/js/2.8d9f2065.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TodoApp/static/js/2.8d9f2065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c473db17d3348bf62a1",
+    "url": "/TodoApp/static/js/main.9e105062.chunk.js"
+  },
+  {
+    "revision": "4d769cb17efc837f2f59",
+    "url": "/TodoApp/static/js/runtime-main.689fce80.js"
+  }
+]);
